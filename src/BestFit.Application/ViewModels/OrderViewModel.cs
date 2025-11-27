eaa61c7a4b0ea13604fb75e547bd14e81f229464
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2025-11-27 12:25:51 +0300

    Imeplemented the Cart, Order and Product view models

---
+++ src/BestFit.Application/ViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using BestFit.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestFit.Application.ViewModels
+{
+    public class OrderViewModel
+    {
+        public OrderProduct  OrderProduct{ get; set; }
+        public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    }
+}
