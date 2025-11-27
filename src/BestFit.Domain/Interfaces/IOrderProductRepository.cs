79c2d73728d337d90014297fe04172dd53a8c572
Jonathan Githumbi <jonathan.m.githumbi@gmail.com>
2025-11-27 09:35:39 +0300

    Created Interfaces all deriving from IRepository and Managed by IUnitOFWork.

---
+++ src/BestFit.Domain/Interfaces/IOrderProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using BestFit.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestFit.Domain.Interfaces
+{
+    public interface IOrderProductRepository : IRepository<OrderProduct>
+    {
+        void UpdateStatus(Guid id,string orderStatus,string? paymentStatus=null);
+    }
+}
---
+++ src/BestFit.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestFit.Domain.Interfaces
+{
+    public interface IUnitOfWork  : IDisposable
+    {
+        IApplicationUserRepository ApplicationUserRepository { get; }
+        ICartRepository CartRepository { get; }
+        ICategoryRepository CategoryRepository { get; }
+        IOrderDetailsRepository OrderDetailsRepository { get; }
+        IOrderProductRepository OrderProductRepository { get; }  
+        IProductRepository ProductRepository { get; }
+        ICustomerMeasurementProfileInterface CustomerMeasurementProfileInterface { get; }
+        IProductMeasurementProfileInterface ProductMeasurementProfileInterface { get; }
+        IProductImageRepository ProductImageRepository { get; }
+        void Save();
+    }
+}
